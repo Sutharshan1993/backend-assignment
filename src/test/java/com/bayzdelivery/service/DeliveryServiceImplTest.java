c1959ed36d49604e4133a7ef64286fac7bcb9ef0
Sutharshan1993 <sutharshanprojectes@gmail.com>
2025-03-11 05:02:44 +0530

    DeliveryServiceImpl Test

--- src/test/java/com/bayzdelivery/service/DeliveryServiceImplTest.java
+++ src/test/java/com/bayzdelivery/service/DeliveryServiceImplTest.java
@@ -1,4 +1,8 @@
 package com.bayzdelivery.service;
 
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
 public class DeliveryServiceImplTest {
 }
